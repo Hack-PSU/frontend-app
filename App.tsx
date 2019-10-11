acae2a8aa929dcbc9fe468660a8a14db797d8f68
Michael Bullington <michael@bullington.xyz>
2019-10-10 20:25:24 -0400

    Initial commit.

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Test 123</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 42,
+    color: 'red'
+  },
+});
