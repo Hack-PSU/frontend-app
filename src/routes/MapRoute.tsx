38fcc027abcdea87ba29accc80135dadc0fef3e5
Rahul Ramkumar <rahulram226@gmail.com>
2019-12-24 11:26:16 -0500

    Add Business Building Image

--- src/routes/MapRoute.tsx
+++ src/routes/MapRoute.tsx
@@ -1,14 +1,36 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 
 import Scaffold from "../components/Scaffold";
 
 const MapRoute: React.FC = () => {
   return (
     <Scaffold title="Map">
-      <View />
+      <View style={styles.root}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/images/BuildingMap.png")}
+        />
+      </View>
     </Scaffold>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    height: "100%",
+    width: "100%",
+    marginTop: 20,
+    marginBottom: 50,
+    justifyContent: "center",
+    flexDirection: "row"
+  },
+
+  image: {
+    flex: 1,
+    height: "80%",
+    resizeMode: "contain"
+  }
+});
 
 export default MapRoute;
