62ee0255af0e8ea11ccd2a27c4f6f7700e968502
Jonathan Tran <jaytran505@gmail.com>
2021-02-14 18:25:47 -0500

    merge conflicts and added button icons

--- src/routes/HomeRoute.tsx
+++ src/routes/HomeRoute.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 
 import React, { useMemo } from 'react'
-import { StyleSheet, View, Linking, ImageComponent } from 'react-native'
-
+import { StyleSheet, View, Linking } from 'react-native'
 import { Text, Title, Button } from 'react-native-paper'
 import Animated from 'react-native-reanimated'
 import * as WebBrowser from 'expo-web-browser'
@@ -20,17 +19,13 @@ import useRegistrationStatus from '../data/hooks/useRegistrationStatus'
 
 import useEvents from '../data/hooks/useEvents'
 
-import { TEXT_LIGHT } from '../theme'
-
+import { TEXT, TEXT_LIGHT } from '../theme'
 
 import Mountain from '../../assets/images/HomeMountains.svg'
 
 import { useDimensions } from 'react-native-hooks'
 
-import { IconButton } from 'react-native-paper'
-
 const REGISTER_URL = 'https://hackpsu.org/register'
-
 
 const DISCORD_URL = 'https://discord.gg/eyPSrNm9Cd'
 const DISCORD_TEXT = `
@@ -77,24 +72,52 @@ const HomeRoute: React.FC = () => {
                 <View style={{ position: 'absolute' }}>
                     <Title style={styles.countdown}>2 hours left!</Title>
 
-                    {/* <IconButton icon="computer" color="#F3603D" style={styles.submitButton} /> */}
-                    <Button mode="contained" dark style={styles.submitButton}>
-                        <Text style={{ color: '#F3603D', fontFamily: 'Cornerstone' }}>Submit</Text>
+                    <Button
+                        onPress={() => Linking.openURL(DEVPOST_URL)}
+                        mode="contained"
+                        dark
+                        style={styles.submitButton}
+                        icon={({ size, color }) => (
+                            <MaterialCommunityIcons
+                                name="hexagon"
+                                size={size}
+                                color={(color = styles.submitButton.color)}
+                            />
+                        )}
+                    >
+                        <Text
+                            style={{
+                                color: styles.submitButton.color,
+                                fontFamily: styles.submitButton.fontFamily,
+                            }}
+                        >
+                            Submit
+                        </Text>
                     </Button>
 
-                    {/* <IconButton icon="mic" style={styles.discordButton} /> */}
                     <Button
                         onPress={() => Linking.openURL(DISCORD_URL)}
                         mode="contained"
                         dark
                         style={styles.discordButton}
+                        icon={({ size, color }) => (
+                            <MaterialCommunityIcons name="discord" size={size} color={color} />
+                        )}
                     >
-                        <Text style={{ color: 'white', fontFamily: 'Cornerstone' }}>Discord</Text>
+                        <Text
+                            style={{
+                                color: TEXT_LIGHT,
+                                fontFamily: styles.discordButton.fontFamily,
+                            }}
+                        >
+                            Discord
+                        </Text>
                     </Button>
                 </View>
 
-
                 <View style={{ backgroundColor: 'white' }}>
+                    <Title style={styles.title}>HOME</Title>
+
                     {registrationStatus.error && <ErrorCard error={registrationStatus.error} />}
 
                     <DateCountDown />
@@ -105,8 +128,8 @@ const HomeRoute: React.FC = () => {
                             <EventWorkshopListItem
                                 model={events[0]}
                                 starEnabled={false}
-                                starItem={() => {}}
-                                onPress={() => {}}
+                                starItem={() => { }}
+                                onPress={() => { }}
                             />
                         </View>
                     )}
@@ -123,16 +146,16 @@ const HomeRoute: React.FC = () => {
                         <HomeListItem description="My PIN" onPress={openRegisterURL}>
                             <View style={styles.buttonContainer}>
                                 {/* {__DEV__ && (
-                                        <Text style={styles.stagingWarning}>
-                                            This **only** shows in development mode. So if you're on
-                                            staging (probably), you can't register since there is no
-                                            staging deployment of HackPSU website lmao. Please setup
-                                            frontend and register for staging hackathon there. I wish
-                                            this could be fixed.
-                                        </Text>
-                                    )} */}
+                    <Text style={styles.stagingWarning}>
+                        This **only** shows in development mode. So if you're on
+                        staging (probably), you can't register since there is no
+                        staging deployment of HackPSU website lmao. Please setup
+                        frontend and register for staging hackathon there. I wish
+                        this could be fixed.
+                    </Text>
+                )} */}
                                 <Button mode="contained" dark>
-                                   Register
+                                    Register
                                 </Button>
                             </View>
                         </HomeListItem>
@@ -140,7 +163,7 @@ const HomeRoute: React.FC = () => {
 
                     <View style={styles.horizontalCardView}>
                         <HomeListItemSecondary
-                           description="Discord"
+                            description="Discord"
                             onPress={() => WebBrowser.openBrowserAsync(DISCORD_URL)}
                         >
                             <Text style={styles.horizontalCardText}>{DISCORD_TEXT}</Text>
@@ -149,9 +172,9 @@ const HomeRoute: React.FC = () => {
                                     mode="outlined"
                                     dark
                                     icon={({ size, color }) => (
-                                       <MaterialCommunityIcons
-                                           name="discord"
-                                           size={size}
+                                        <MaterialCommunityIcons
+                                            name="discord"
+                                            size={size}
                                             color={color}
                                         />
                                     )}
@@ -168,7 +191,7 @@ const HomeRoute: React.FC = () => {
                             <Text style={styles.horizontalCardText}>{DEVPOST_TEXT}</Text>
                             <View style={styles.buttonContainer}>
                                 <Button
-                                   mode="outlined"
+                                    mode="outlined"
                                     dark
                                     icon={({ size, color }) => (
                                         <MaterialCommunityIcons
@@ -178,52 +201,6 @@ const HomeRoute: React.FC = () => {
                                         />
                                     )}
                                 >
-                                    Open
-                                </Button>
-
-                        </View>
-                    )}
-
-                    {!registrationStatus.error && !registrationStatus.data && (
-                        <HomeListItem description="My PIN Number" onPress={openRegisterURL}>
-                            <View style={styles.buttonContainer}>
-                                {__DEV__ && (
-                                    <Text style={styles.stagingWarning}>
-                                        This **only** shows in development mode. So if you're on
-                                        staging (probably), you can't register since there is no
-                                        staging deployment of HackPSU website lmao. Please setup
-                                        frontend and register for staging hackathon there. I wish
-                                        this could be fixed.
-                                    </Text>
-                                )}
-                                <Button mode="contained" dark>
-                                    Register
-                                </Button>
-                            </View>
-                        </HomeListItem>
-                    )}
-
-                    <View style={styles.horizontalCardView}>
-                        {!registrationStatus.error && (
-                            <HomeListItem
-                                description="My PIN Number"
-                                info={
-                                    !registrationStatus.data
-                                        ? '...'
-                                        : registrationStatus.data.pin.toString()
-                                }
-                            />
-                        )}
-
-                        <HomeListItemSecondary
-                            description="Discord"
-                            onPress={() => Linking.openURL(DISCORD_URL)}
-                        >
-                            <Text style={styles.horizontalCardText}>
-                                Request an invite by clicking here!
-                            </Text>
-                            <View style={styles.buttonContainer}>
-                                <Button mode="contained" dark>
                                     Open
                                 </Button>
                             </View>
@@ -238,10 +215,10 @@ const HomeRoute: React.FC = () => {
 const styles = StyleSheet.create({
     title: {
         fontFamily: 'Cornerstone',
-        color: TEXT_LIGHT,
+        color: 'black',
         fontSize: 48,
         lineHeight: 54,
-        paddingTop: 22 + LOGO_SAFE_PADDING,
+        paddingTop: 16,
         paddingBottom: 16,
         paddingLeft: 16,
     },
@@ -272,7 +249,7 @@ const styles = StyleSheet.create({
         paddingTop: 8,
         paddingBottom: 16,
 
-        color: 'white',
+        color: 'black',
         fontSize: 20,
         lineHeight: 24,
         fontFamily: 'Plex-Mono',
@@ -303,6 +280,8 @@ const styles = StyleSheet.create({
         width: 120,
         top: 350,
         left: 42,
+        color: '#F3603D',
+        fontFamily: 'Cornerstone',
     },
 
     discordButton: {
@@ -310,6 +289,7 @@ const styles = StyleSheet.create({
         width: 120,
         top: 317,
         left: 190,
+        fontFamily: 'Cornerstone',
     },
 })
 
