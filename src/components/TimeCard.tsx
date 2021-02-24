cfa895b2b4f7a4ed36e8dc87a282727614d9a207
Kalp Patel <kalppatel.3311@gmail.com>
2021-02-24 17:36:24 -0500

    Creating component for TimeCard, and moving countDown up

---
+++ src/components/TimeCard.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { Card, Title, Headline } from 'react-native-paper'
+
+import { TEXT_LIGHT } from '../theme'
+
+interface Props {
+    description?: string
+    info?: string
+}
+
+const TimeCard: React.FC<Props> = (props) => {
+    return (
+        <Card style={styles.card}>
+            <Card.Content>
+                {props.description && (
+                    <Title style={styles.description}>{props.description}:</Title>
+                )}
+                {props.info && <Headline style={styles.info}>{props.info}</Headline>}
+            </Card.Content>
+        </Card>
+    )
+}
+
+const styles = StyleSheet.create({
+    card: {
+        marginBottom: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        backgroundColor: 'transparent',
+    },
+
+    description: {
+        marginBottom: 10,
+        opacity: 0.87,
+        fontSize: 20,
+        color: TEXT_LIGHT,
+    },
+
+    info: {
+        fontSize: 30,
+        color: TEXT_LIGHT,
+    },
+})
+
+export default TimeCard
