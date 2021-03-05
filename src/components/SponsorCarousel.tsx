cb0dd668dab4bdac72f9a62cf210d16c1ee79707
Kalp Patel <kalppatel.3311@gmail.com>
2021-03-04 19:22:51 -0500

    Update EECS logo

--- src/components/SponsorCarousel.tsx
+++ src/components/SponsorCarousel.tsx
@@ -9,26 +9,25 @@ import ICS_SVG from '../../assets/images/sponsors/ICS-day-cropped.svg'
 import LION_LAUNCHPAD_SVG from '../../assets/images/sponsors/Lion-Launchpad.svg'
 import HVC_SVG from '../../assets/images/sponsors/HVC.svg'
 
-//assets/images/sponsors/Lion-Launchpad-LOGO.svg
 const MICROSOFT_URL = 'https://www.microsoft.com/'
 const EECS_URL = 'https://www.eecs.psu.edu/'
 const ICS_URL = 'https://www.icds.psu.edu/'
 const LION_LAUNCHPAD_URL = 'https://lionlaunchpad.psu.edu/'
 const HVC_URL = 'https://www.linkedin.com/company/happy-valley-communications/'
-//onPress={() => Linking.openURL(DEVPOST_URL)}
+// const HVC_URL = 'https://happyvalleycommunications.psu.edu/'
+
 const SponsorCarousel: React.FC = () => {
     return (
         <View style={styles.wrapper}>
             <Slick
-                // onMomentumScrollEnd={(e, state, context) => console.log('index:', state.index)}
                 height={110}
                 dot={<View style={styles.dot} />}
                 activeDot={<View style={styles.activeDot} />}
                 paginationStyle={styles.pagination}
-                //autoplay
-                //autoplayTimeout={2}
+                autoplay
+                autoplayTimeout={2}
                 horizontal
-                //loop
+                loop
             >
                 <View style={styles.logoContainer}>
                     <TouchableOpacity onPress={() => Linking.openURL(MICROSOFT_URL)}>
@@ -37,7 +36,7 @@ const SponsorCarousel: React.FC = () => {
                 </View>
                 <View style={styles.logoContainer}>
                     <TouchableOpacity onPress={() => Linking.openURL(EECS_URL)}>
-                        <EECS_SVG style={styles.bigLogo} />
+                        <EECS_SVG style={styles.logo} />
                     </TouchableOpacity>
                 </View>
                 <View style={styles.logoContainer}>
@@ -62,14 +61,6 @@ const SponsorCarousel: React.FC = () => {
 
 const styles = StyleSheet.create({
     wrapper: { height: 130 },
-    test: {
-        height: 10,
-        width: 300,
-    },
-    test2: {
-        height: 10,
-        width: 10,
-    },
     dot: {
         backgroundColor: 'rgba(0,0,0,.2)',
         width: 6,
@@ -100,15 +91,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    smallerLogo: {
-        height: 120,
-        width: 250,
-    },
-    smallLogo: {
-        height: 80,
-        width: 300,
-        top: 10,
-    },
     logo: {
         flex: 1,
         top: 10,
@@ -116,15 +98,6 @@ const styles = StyleSheet.create({
         width: 300,
         alignItems: 'center',
         justifyContent: 'center',
-    },
-    bigLogo: {
-        flex: 1,
-        height: 110,
-        width: 300,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        marginTop: 'auto',
-        marginBottom: 'auto',
     },
 })
 
